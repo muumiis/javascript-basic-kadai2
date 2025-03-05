a07f5dc9642b570ae2f6a390698fd08973d422cb
muumiis <p.ssss23yuu@gmail.com>
2025-03-05 15:11:49 +0900

    Add files via upload
    a

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
-btn,addEventListener('click',() => {
+btn.addEventListener('click',() => {
   text.textContent = "ボタンをクリックしました"
 });
 
